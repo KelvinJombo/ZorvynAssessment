b659fe83635c90f5f2cfb64e12b943ab8ae66937
Kelvin Okonkwo <kelly2000s@yahoo.com>
2026-04-03 06:40:17 +0100

    Clean Architecture Setup

---
+++ FinanceDashboard.Api/FinanceDashboard.Domain/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using FinanceDashboard.Domain.Models.Enums;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinanceDashboard.Domain.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public UserRole Role { get; set; }
+
+        // Navigation
+        public ICollection<FinancialRecord> FinancialRecords { get; set; } = new List<FinancialRecord>();
+    }
+}
